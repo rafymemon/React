b5553f0c49d1d978328bcf8e0a05aa8bd2a3afaf
Rafy Mustafa <128308433+rafymemon@users.noreply.github.com>
2025-04-03 19:10:32 +0500

    Creating a new react library

--- basics_vite/src/main.jsx
+++ basics_vite/src/main.jsx
@@ -1,10 +1,35 @@
+import ReactDom from "react-dom/client";
+import App from "./App.jsx";
+import React from "react";
 
-import { createRoot } from 'react-dom/client'
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  );
+}
+// const reactElement = {
+//     type: "a",
+//     props: {
+//       href: "https://www.google.com",
+//       target: "_blank",
+//     },
+//     children: "Click me to visit google",
+//   };
 
-import App from './App.jsx'
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "https://www.google.com",
+    target: "_blank",
+  },
+  "Click me to visit google"
+);
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google{" "}
+  </a>
+);
 
-createRoot(document.getElementById('root')).render(
-  
-    <App />
- 
-)
+ReactDom.createRoot(document.getElementById("root")).render(reactElement);
---
+++ customReact/customreact.js
@@ -0,0 +1,29 @@
+function customRender(element, container) {
+  /*
+    const domElement = document.createElement
+    (element.type)
+    domElement.innerHTML = element.Children 
+    domElement.setAttribute("href", element.props.href)
+    domElement.setAttribute('target', element.props.target)
+
+    container.appendChild(domElement) */
+
+  const domElement = document.createElement(element.type);
+  domElement.innerHTML = element.children;
+  for (const prop in element.props) {
+    if (prop === 'children') continue;
+    domElement.setAttribute(prop, element.props[prop]);
+  }
+  container.appendChild(domElement)
+}
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://www.google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+const mainContainer = document.querySelector("#root");
+customRender(reactElement, mainContainer);
